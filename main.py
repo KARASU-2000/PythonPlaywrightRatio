2aa2ba13fd52eb2840ac4df0104acc4d8ef7793b
YUUKI <YUUKI>
2022-06-12 20:49:50 +0900

    [add]close #9 脚本家毎の割合を出す機能を追加

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ try:
     # ファイルパスを取得する
     path = input("読み込むファイルのパスを入力：")
     # Excelファイルを読み込んで整形する
-    result = operation_excel.formatting_excel(operation_excel.loading_excel(path))
+    excel = operation_excel.formatting_excel(operation_excel.loading_excel(path))
     # 脚本家毎の担当話数を表示する
-    print(analysis.counting_playwright(result))
+    print(analysis.calculating_playwright_ratio(excel))
 except Exception as e:
     # エラーを表示する
     print(e)
