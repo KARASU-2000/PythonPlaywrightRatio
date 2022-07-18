88f561a316ca787e8d044c55d23a9100a37d583e
YUUKI <YUUKI>
2022-07-18 11:21:40 +0900

    [add]close #16 円グラフ出力機能を追加

--- main.py
+++ main.py
@@ -5,22 +5,15 @@
 # モジュールのインポート
 import operation_excel
 import analysis
+import draw_graph
 
 try:
     # ファイルパスを取得する
     path = input("読み込むファイルのパスを入力：")
     # Excelファイルを読み込んで整形する
     excel = operation_excel.formatting_excel(operation_excel.loading_excel(path))
-
-    # 脚本家一覧を表示する
-    list = analysis.calculating_playwright_ratio(excel)
-    print("脚本家一覧")
-    for row in list:
-        print("　" + row[analysis.COLUMN_PLAYWRIGHT])
-
-    # メイン脚本家の割合を表示する
-    playwright = input("メイン脚本家名を入力：")
-    print(str(analysis.calculating_main_ratio(excel, playwright)) + "%")
+    # 脚本家の割合を円グラフで表示する
+    draw_graph.drawing_pie_chart(analysis.calculating_playwright_ratio(excel))
 except Exception as e:
     # エラーを表示する
     print(e)
